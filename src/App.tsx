46eef6df285a685d43ad4ab5edb43c1f59f84b5b
Hamdi Kamel <hamdiakippuden@gmail.com>
2024-03-27 05:26:07 -0700

    added project files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="font-sans-serif" role='main'>
+      <h1 className="font-bold underline text-primary-light-red">Just setting things up for the project.</h1>
+    </div>
+  )
+}
+
+export default App
